b969c600d851da8170a221a4f86e96d4251b6953
andorsk <andor.developer@gmail.com>
2017-11-01 14:15:52 -0700

    removed main

--- main/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import u "andortracker/controller/user"
-
-func main() {
-	a := u.App{}
-	a.Initialize("root", "c0raline", "rest-api-example")
-	a.Run(":8080")
-}
---
+++ test/README.md
@@ -0,0 +1,1 @@
+Debating whether to make this happen. This should theoretically be a subfolder to hold the tests. Thinking about keeping them in the same directory instead
